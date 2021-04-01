587aa5d4f546dc835b4cd4ffcd0fe393081166a9
Patrice LaFlamme <patrixl@gmail.com>
2021-04-01 13:35:45 +0900

    revert to solarized

--- personal/preload/themes.el
+++ personal/preload/themes.el
@@ -1,2 +1,8 @@
 ;; (setq prelude-theme 'doom-monokai-pro)
-(setq prelude-theme 'modus-vivendi)
+
+;;(setq modus-themes-mode-line '3d
+;;      modus-themes-paren-match 'intense-bold)
+
+;;(setq prelude-theme 'modus-vivendi)
+
+(setq prelude-theme 'doom-solarized-light)
